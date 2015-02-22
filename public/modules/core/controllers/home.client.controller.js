a852d8b968735ee4b4cbf182bed4904d8db39f35
Jean-Ralph Aviles <jraviles@JR-Macbook.local>
2015-02-21 21:00:22 -0500

    random button

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -1,9 +1,14 @@
 'use strict';
 
-
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-	function($scope, Authentication) {
-		// This provides Authentication context.
+angular.module('core').controller('HomeController', ['$scope', '$location', 'Authentication', 'Events',
+	function($scope, $location, Authentication, Events) {
 		$scope.authentication = Authentication;
+
+    $scope.getRandomEvent = function() {
+      var events = Events.query(function(res) {
+        var item = res[Math.floor(Math.random()*res.length)];
+        $location.path('events/' + item._id);
+      });
+    }
 	}
 ]);
